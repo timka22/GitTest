5b2ab31a33e34c474151c7d59d4c6fa857b66faa
root <root@vm526489.u162190.kvm.ubuntu-20.04-amd64-r1>
2025-10-13 09:18:19 +0000

    Практика 5

---
+++ api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from todo import todo_router
+from model import Todo
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome() -> dict:return { "message": "Hello World"}
+
+app.include_router(todo_router)
---
+++ model.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import List
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {"item": "Example schema 1!"},
+                    {"item": "Example schema 2!"}
+                ]
+            }
+        }
